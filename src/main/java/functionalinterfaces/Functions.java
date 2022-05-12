4b24cc0ce70a5ed16282974274e4c6fa10a7d8ef
Olivier Loukombo <olivierloukombo@gmail.com>
2022-05-12 21:12:08 +0100

    chain function with andThen method

--- src/main/java/functionalinterfaces/Functions.java
+++ src/main/java/functionalinterfaces/Functions.java
@@ -6,9 +6,15 @@ public class Functions {
     public static void main(String [] args){
 
         System.out.println(incrementByOneFunction.apply(1));
+        System.out.println(multiplyBy10.apply(5));
+        System.out.println(addByOneAndThenMultiply.apply(10));
     }
     //declarative
-    static Function<Integer, Integer> incrementByOneFunction = number -> number++;
+    static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+    static Function<Integer, Integer> multiplyBy10 = number -> number * 10;
+    static Function<Integer, Integer> addByOneAndThenMultiply = incrementByOneFunction.andThen(multiplyBy10);
+
+
     //imperative
     public static int increment(int num){
         return num + 1;
