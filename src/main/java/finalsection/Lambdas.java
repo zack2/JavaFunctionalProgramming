a1850703b402ded10a05bc23352fa965b41a4618
Olivier Loukombo <olivierloukombo@gmail.com>
2022-05-16 10:09:57 +0100

    add lambda and update

--- src/main/java/finalsection/Lambdas.java
+++ src/main/java/finalsection/Lambdas.java
@@ -10,21 +10,22 @@ public class Lambdas {
         //with method reference
         Function<String, String> upperCaseNameWithMethodRef = String::toUpperCase;
 
+        /*
+         *
+         * the first generic Type (the input eg:String) is for the first parameter
+         * The second generic type (the output eg:Integer) is for the second parameter
+         * the third one is for the return type
+         **/
+         BiFunction<String, Integer, String> upperCaseWithBiFunction = (name, age) ->{
+            if(name.isBlank())
+                throw  new IllegalArgumentException("");
+            System.out.println(age);
+            return name.toLowerCase();
+        };
         System.out.println(upperCaseWithBiFunction.apply("Alex", 20));
 
 
     }
 
-    /*
-     *
-     * the first generic Type (String) is for the first parameter
-     * The second generic type (Integer) is for the second parameter
-     * the third one is for the return type
-     **/
-    static BiFunction<String, Integer, String> upperCaseWithBiFunction = (name, age) ->{
-        if(name.isBlank())
-            throw  new IllegalArgumentException("");
-        System.out.println(age);
-        return name.toLowerCase();
-    };
+
 }
